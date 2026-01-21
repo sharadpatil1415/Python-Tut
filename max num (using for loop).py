c8ea2b0c91caf0b379d4a67bc16eadf85bcc8af4
sharad patil <sdpatil7419@gmail.com>
2026-01-21 20:26:53 +0530

    feat: Add script to find the maximum number in a list and a basic random password generator.

---
+++ max num (using for loop).py
@@ -0,0 +1,7 @@
+student_scores = [85, 92, 78, 90, 88, 76, 95, 89]
+
+max_score = 0
+for score in student_scores :
+    if score > max_score :
+       max_score = score 
+print(max_score)
--- projects/password manager(basic).py
+++ projects/password manager(basic).py
@@ -1,4 +1,40 @@
+from random import random
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '<', '>', ';', ':','\\', '/']
+
+n_letters = int(input("how many letters you want in your password :"))
+n_numbers = int(input("how many numbers you want in your password :"))
+n_symbols = int(input("how many symbols you want in your password :"))
+
+
+password = []
+
+for char in range(0, n_letters) :
+    password.append (random.choice(letters))
+
+for char in range(0, n_numbers) :
+    password.append (random.choice(numbers))
+
+for char in range(0, n_symbols) :
+    password.append (random.choice(symbols))
+
+print(password)
+random.shuffle(password)
+
+secure_password = " " 
+
+for char in password :
+    secure_password += char
+
+print(secure_password)
+    
+
+    
+          
+
+
+
 
